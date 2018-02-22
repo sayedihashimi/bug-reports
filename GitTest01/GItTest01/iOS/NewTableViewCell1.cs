56e61e383dcca434b13836b1008724153738fb02
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-02-22 11:18:59 -0500

    adding files

---
+++ GitTest01/GItTest01/iOS/NewTableViewCell1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace GItTest01.iOS
+{
+    public partial class NewTableViewCell1 : UITableViewCell
+    {
+        public static readonly NSString Key = new NSString("NewTableViewCell1");
+        public static readonly UINib Nib;
+
+        static NewTableViewCell1()
+        {
+            Nib = UINib.FromName("NewTableViewCell1", NSBundle.MainBundle);
+        }
+
+        protected NewTableViewCell1(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
