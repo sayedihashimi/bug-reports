8b25d7dcc9eaaa3bd2f705f979435de2a7cea578
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-03-05 11:30:00 -0500

    changing class

---
+++ Refactor01/Refactor01/Refactor01/Contacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor01
+{
+    public class MyOtherClass01
+    {
+        public IList<Person> Contacts = new List<Person>();
+
+        public MyOtherClass01()
+        {
+            Contacts.Add(new Person());
+            Contacts.Add(new Person("name", "street 1"));
+        }
+    }
+}
---
+++ Refactor01/Refactor01/Refactor01/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor01
+{
+    public class Person
+    {
+        public string Name {
+            get;set;
+        }
+        public string Street1 {
+            get;set;
+        }
+        public Person()
+        {
+        }
+        public Person(string name, string street1)
+        {
+            Name = name;
+            Street1 = street1;
+        }
+    }
+}
