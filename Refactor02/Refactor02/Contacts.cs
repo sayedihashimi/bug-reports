39acd656034227106b55f3e8df5598bb9ea400cf
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-03-05 16:12:36 -0500

    adding new project

---
+++ Refactor02/Refactor02/Contacts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor02
+{
+    public class Contacts
+    {
+        public IList<Person> contacts = new List<Person>();
+
+        public Contacts()
+        {
+            contacts.Add(new Person());
+            contacts.Add(new Person("name", "street 1"));
+        }
+
+        public IList<Person> GetContacts()
+        {
+            return contacts;
+        }
+
+        public void AddContact(Person contact)
+        {
+            contacts.Add(contact);
+        }
+    }
+}
---
+++ Refactor02/Refactor02/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Refactor02
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
