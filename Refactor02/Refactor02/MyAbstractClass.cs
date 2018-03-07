a550cffd76ea8010c7f27325559950bb0690f1f7
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-03-07 16:35:31 -0500

    adding abstract class

---
+++ Refactor02/Refactor02/MyAbstractClass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Refactor02
+{
+    public abstract class MyAbstractClass
+    {
+        public abstract void UpdatePerson(Person p, string name, string street1);
+        public abstract Person GetPersonByname(string name);
+    }
+}
---
+++ Refactor02/iOS/MyOtherClass.cs
@@ -0,0 +1,11 @@
+﻿namespace Refactor02
+{
+    public class MyOtherClass
+    {
+        private string foo;
+        private string bar;
+
+        public string Name
+        { get; set; }
+    }
+}
