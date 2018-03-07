2e20df8d285a0bd7515c67b223e9dad6f644c48b
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-03-07 16:38:36 -0500

    adding interface

---
+++ Refactor02/Refactor02/IMyInterface.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Refactor02
+{
+    public interface IMyInterface{
+        void UpdatePerson(Person p, string name, string street1);
+        Person GetPersonByName(string name);
+    }
+}
