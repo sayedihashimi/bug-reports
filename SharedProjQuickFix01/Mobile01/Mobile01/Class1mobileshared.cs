98b31603b86495f698427322253bba1d55223033
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-03-07 13:59:14 -0500

    adding new solution

---
+++ SharedProjQuickFix01/Mobile01/Mobile01/Class1mobileshared.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobile01
+{
+    public class Class1mobileshared
+    {
+        public void Test()
+        {
+            // Before
+            int x;
+            if (1==1)
+            {
+                x = 1;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ SharedProjQuickFix01/SharedProject1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            // Before
+            int x;
+            if (1==1)
+            {
+                x = 1;
+                Console.WriteLine(x);
+            }
+        } 
+    }
+}
