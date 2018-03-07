95f173feebc42cb7c54a988118107ba72467b83c
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-03-07 15:18:37 -0500

    adding shared project

---
+++ SharedProject1/SharedProject1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            int x;
+            if (condition)
+            {
+                x = 1;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
